2d0ca0f2d18799847cebe5617d8a262b01d32377
Juan Carlos Palomo <juancarlos.palomo@gmail.com>
2016-01-15 12:57:46 +0100

    string data type

--- javascript/datatypes/strings.js
+++ javascript/datatypes/strings.js
@@ -1,8 +1,28 @@
-//Example of creating a variable for the string type
+//Example of creating & using a variable for the string type
 
-//using strict mode every var has to be declared with var, before used it.  
-//See strictmode.js script
+//using strict mode, every var has to be declared with var, before used it.  
+//see strictmode.js script
 'use strict';  
 
 var name = 'Jack';
 console.log(name);
+
+//operate with string to concat a value.
+//cast the 85 number to string to concat it
+name = 'Jack ' + 85;
+console.log(name);
+
+//double quotes can be used too to assign a string value to a variable
+name = "Jack";
+console.log(name);
+
+//you can use single quotes inside double quotes.  
+//It is very useful for instance with genitive saxon
+name = "Then Jack's name";
+console.log(name);
+
+//variables can be dynamically change their types
+//with only assign it a value of another type 
+console.log(typeof(name)); //string type
+name = 1;
+console.log(typeof(name)); //number type
