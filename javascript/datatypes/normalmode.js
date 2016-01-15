fae0596485fa4bf7169a7d6e34d12cee426aef07
Juan Carlos Palomo <juancarlos.palomo@gmail.com>
2016-01-15 12:22:38 +0100

    strict & normal modes

---
+++ javascript/datatypes/normalmode.js
@@ -0,0 +1,5 @@
+//We don't use strict mode, so this will work fine
+//although name var is not declared
+name = 'Jack';
+console.log(name);
+
--- javascript/datatypes/strictmode.js
+++ javascript/datatypes/strictmode.js
@@ -5,8 +5,10 @@ try {
     name = 'Jack'; //this line will fail as name as not declared;
     console.log(name);
 } catch (e) {
-    console.log ('name is not defined ' + e.message);
+    console.log (e.message);
 }
 
-
+//Now will declare a var and it will not fail
+var surname = 'Nicholson';
+console.log(surname);
 
