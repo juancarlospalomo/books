e68b7fb5e805a4b957ea7b4e614a72f3378e6aaa
Juan Carlos Palomo <juancarlos.palomo@gmail.com>
2017-05-21 17:47:31 +0200

    Arrays

---
+++ c#/ArrayCosts/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ArrayCosts
+{
+    class Program
+    {
+        private static int[] _costs;
+
+        private static void init()
+        {
+            _costs = new int[5] { 10, 15, 20, 25, 30 };
+        }
+
+        private static void reset()
+        {
+            _costs[0] = 0;
+            _costs[1] = 0;
+            _costs[2] = 0;
+            _costs[3] = 0;
+            _costs[4] = 0;
+        }
+
+        private static void changeValues(ref int[] values)
+        {
+            values[0] = 10 * values[0];
+            values[1] = 10 * values[1];
+            values[2] = 10 * values[2];
+            values[3] = 10 * values[3];
+            values[4] = 10 * values[4];
+        }
+
+        static void Main(string[] args)
+        {
+            init();
+            foreach (int value in _costs)
+            {
+                Console.WriteLine($"value = {value}");
+            }
+            Console.WriteLine("Changing values");
+            changeValues(ref _costs);
+            foreach (int value in _costs)
+            {
+                Console.WriteLine($"value = {value}");
+            }
+        }
+    }
+}
