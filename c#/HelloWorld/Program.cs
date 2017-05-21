c264e743c830ede7a3ff803d5581b043a53c10f6
Juan Carlos Palomo <juancarlos.palomo@gmail.com>
2017-05-21 15:09:32 +0200

    c#

---
+++ c#/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name followed by enter: ");
+            string word = Console.ReadLine();
+            Console.WriteLine($"Hello from the World, {word}");
+        }
+    }
+}
