4df4f2e48a667eeddb0c05d93513dd080e26ba8c
juancarlos <jcpalomo@evendor.es>
2017-05-25 11:25:09 +0200

    Recipe Service

--- c#/Food2ForkApiClient/Services/RecipeServices.cs
+++ c#/Food2ForkApiClient/Services/RecipeServices.cs
@@ -7,14 +7,23 @@ namespace Food2ForkApiClient
 
     public class RecipeServices
     {
+        private enum CommandName
+        {
+            Get,
+            Search
+        };
+
         //Example of GET Request: http://food2fork.com/api/search?key=16fb9daf53cd0cd3a321933ec630f2a1&q=cocido
         private const string API_KEY = "16fb9daf53cd0cd3a321933ec630f2a1";
         private const string API_KEY_PARAM_NAME = "key";
         private const string SEARCH_PARAM_NAME = "q";
         private const string URL_API = "http://food2fork.com/api/";
         private const string URL_API_SEARCH = "search";
+        private const string URL_API_GET = "get";
+        
         //TODO: Remove
-        public string BuildUrl() {
+        public string BuildUrl()
+        {
             return string.Format($"{URL_API}");
         }
 
