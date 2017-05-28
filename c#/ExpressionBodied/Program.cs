dca8bda89188844cb7d5b5bfe467632c4d39c009
Juan Carlos Palomo <juancarlos.palomo@gmail.com>
2017-05-28 15:55:27 +0200

    Lambda expressions

---
+++ c#/ExpressionBodied/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExpressionBodied
+{
+
+    delegate string Say(string name);
+
+    class Program
+    {
+        static int sum (int a, int b) => a + b;
+        static int number => 10;
+
+        static string SayHello(string name)
+        {
+            return $"Hello {name}";
+        }
+
+        static string BuildMessage(Say message)
+        {
+            return message("New Message");
+        }
+
+        private static void DoSomething(Action action ) {
+            action();
+        }
+
+        static void Main(string[] args)
+        {
+
+            Say say = SayHello;
+
+            Console.WriteLine(say("John"));
+
+            Say sayGoodBye = name => { return $"GoodBye {name}"; };
+
+            Console.WriteLine(BuildMessage(message => message.ToUpper()));
+
+            Console.WriteLine($"sum(3,4): {sum(3, 4)}");
+
+            Action<int> action = x => Console.WriteLine(x);
+
+            action(10);
+
+            DoSomething(() => {
+                Console.WriteLine($"test");
+            });
+
+        }
+    }
+}
